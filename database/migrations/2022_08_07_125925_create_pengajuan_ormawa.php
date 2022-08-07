34ab6e20290cd4a48f58643398ef92afe31c71be
Syeh Biaggie <biaggiesyeh15@gmail.com>
2022-08-07 20:07:08 +0700

    Update syeh

---
+++ database/migrations/2022_08_07_125925_create_pengajuan_ormawa.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengajuanOrmawa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengajuan_ormawa', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('ormawa_id');
+            $table->string('nama_kegiatan');
+            $table->string('jenis_kegiatan');
+            $table->string('tema_kegiatan');
+            $table->dateTime('tanggal_kegiatan');
+            $table->double('total_dana', 8, 2);
+            $table->string('lampiran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengajuan_ormawa');
+    }
+}
