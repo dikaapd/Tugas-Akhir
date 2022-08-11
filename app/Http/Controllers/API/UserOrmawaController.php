321b1f21eb35abca587db843c7c9a94d1011490f
dikaapd <dikaprasetyad77@gmail.com>
2022-08-11 22:02:08 +0700

    'updateAPI'

---
+++ app/Http/Controllers/API/UserOrmawaController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserOrmawaController extends Controller
+{
+    public function register(Request $req)
+    {
+        $rules = [
+            'nama' => 'required',
+            'username' => 'required|string|unique:users',
+            'password' => 'required|string|min:6'
+        ];
+        $validator = Validator::make($req->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::create([
+            'nama' => $req->nama,
+            'username' => $req->username,
+            'password' => Hash::make($req->password)
+        ]);
+        $token = $user->createToken('Personal Access Token')->plainTextToken;
+        $response = ['user' => $user, 'token' => $token];
+        return response()->json($response, 200);
+    }
+
+    public function login(Request $req)
+    {
+        $rules = [
+            'username' => 'required',
+            'password' => 'required|string'
+        ];
+        $req->validate($rules);
+
+        $user = User::where('username', $req->username)->first();
+
+        if ($user && Hash::check($req->password, $user->password)) {
+            $token = $user->createToken('Personal Access Token')->plainTextToken;
+            $response = ['user' => $user, 'token' => $token];
+            return response()->json($response, 200);
+        }
+        $response = ['message' => 'Incorrect Username dan Password'];
+        return response()->json($response, 400);
+    }
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
